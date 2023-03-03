2ea86934203684f9089318d639ce80cee4c50b60
Hoang Khanh <khanh58k1@gmail.com>
2023-03-03 09:35:31 +0700

    upload code

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function showHideMysef(){
+   let addMysef = document.getElementById('mysef');
+   addMysef.classList.toggle('show');
+}
+
+function showBell(id){
+    let showTab = document.getElementById('show_tab');
+    showTab.classList.toggle(id);
+
+}
+function showCompact(){
+    let showCompact = document.getElementById('id-main');
+    showCompact.classList.toggle('show-compact');
+}
