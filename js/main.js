6553d2c3143d534ca91635fe7a154081b616ed6d
Hoang Khanh <khanh58k1@gmail.com>
2023-03-06 11:21:59 +0700

    test github

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
+//test github
 function showHideMysef(){
    let addMysef = document.getElementById('mysef');
    addMysef.classList.toggle('show');
 }
-
 function showBell(id){
     let showTab = document.getElementById('show_tab');
     showTab.classList.toggle(id);
-
 }
 function showCompact(){
     let showCompact = document.getElementById('id-main');
